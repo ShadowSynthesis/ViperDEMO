ff39062433bfad12d31b64c6d64915201946cae9
Yoram <yoram.soussan@adopteunmec.com>
2024-08-23 13:49:22 +0200

    remove useless info from entity

--- ViperDEMO/ViperDEMO/Entity.swift
+++ ViperDEMO/ViperDEMO/Entity.swift
@@ -12,29 +12,13 @@ struct Geo: Codable {
     let lng: String
 }
 
-struct Address: Codable {
-    let street: String
-    let suite: String
-    let city: String
-    let zipcode: String
-    let geo: Geo
-}
-
-struct Company: Codable {
-    let name: String
-    let catchPhrase: String
-    let bs: String
-}
-
 struct User: Codable {
     let id: Int
     let name: String
     let username: String
     let email: String
-    let address: Address
     let phone: String
     let website: String
-    let company: Company
 }
 
 enum FetchError: Error {
